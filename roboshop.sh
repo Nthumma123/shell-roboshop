ce58e77a5a6af16b03e033ed0f58789c51daf0bc
neeli <neelimareddygopu1@gmail.com>
2026-01-31 16:36:55 +0530

    create instances and update records

---
+++ roboshop.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#requirements:
+
+#Variables Declaration
+SG_ID="sg-04afc53a2f45b85cc"
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z0208373RVMHCU0ECIYA"
+DOMAIN_NAME="neelimadevops.online"
+
+# We are creating Instance and records in Route53
+
+
+for instance in $@
+do 
+   INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-group-ids $SG_ID \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$instance'}]' \
+    --query 'Instances[0].InstanceId' \
+    --output text )
+
+
+    if [ $instance == frontend] ; then
+        IP=$(aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+            )
+        RECORD_NAME = "$DOMAIN_NAME"
+
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    fi
+    
+    echo "IP Address: $IP"
+
+    aws route53 change-resource-record-sets 
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+                {
+                "Comment": "Update CNAME record for test.example.com",
+                "Changes": [
+                    {
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": "'$RECORD_NAME'",
+                        "Type": "CNAME",
+                        "TTL": 1,
+                        "ResourceRecords": [
+                        {
+                            "Value": "'$IP'"
+                        }
+                        ]
+                    }
+                    }
+                ]
+                }
+                '
+    echo "record updated for $instance"
+
+done
